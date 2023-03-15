eeab290cd7610ab2f160c7d8a92f8fd2e72ad01c
abeerAyad <abeerayyad2000@gmail.com>
2023-03-15 02:00:01 +0200

    edit styling Relates#16

--- indexFiles/index.js
+++ indexFiles/index.js
@@ -1,46 +1,29 @@
-
 const sliderImages = Array.from(document.querySelectorAll('.container img'));
+const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('prev');
 const slidesCount = sliderImages.length;
 
 let currentSlide = 1;
-const slideNumberElement = document.getElementById('slide-number');
-
-const nextButton = document.getElementById('next');
-const prevButton = document.getElementById('prev');
-
-
-
-const removeAllActive =() => {
-  sliderImages.forEach( (img) => img.classList.remove('active'));
-}
 
 const theChecker = () => {
-
-  removeAllActive();
-  sliderImages[currentSlide - 1].classList.add('active');
-  currentSlide == 1 ? prevButton.classList.add('disabled'):prevButton.classList.remove('disabled');
+sliderImages[currentSlide -1].classList.add('active');
+   currentSlide == 1 ? prevButton.classList.add('disabled')
+   :prevButton.classList.remove('disabled');
   
   currentSlide == slidesCount ? nextButton.classList.add('disabled')
   :nextButton.classList.remove('disabled');
+  if(currentSlide === 1) {
+    currentSlide = 1
+  }
+   if(currentSlide === slidesCount) {
+    currentSlide = slidesCount
+    clearInterval(myInterval)
 
+  }
+  currentSlide++
 }
-const nextSlide = () => {
-  nextButton.classList.contains('disabled')
-  ? false
-  :currentSlide++;
-    theChecker();
-}
+
+const myInterval = setInterval(theChecker,1000)
 
 
-const  prevSlide = () => {
-  prevButton.classList.contains('disabled') 
-  ? false
-  :currentSlide--;
-    theChecker();
-}
-// theChecker();
 
-nextButton.addEventListener("click",nextSlide);
-prevButton.addEventListener("click",prevSlide);
-
-setTimeout(theChecker,10)
